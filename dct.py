6cf6c8f9da8ccaf35c889a9c679be46c0dfde709
Charlie Jackson <71543803+charlieojackson91@users.noreply.github.com>
2021-01-05 14:56:56 +0000

    Add files via upload

---
+++ dct.py
@@ -0,0 +1,11 @@
+category_dct = {
+        
+        'user': {'family' : ['family', 'families', 'kid', 'toddler', 'baby', 'babies', 'teen', 'teenager', 'child'], 'couples' : ['couple', 'boyfriend', 'girlfriend', 'wife', 'husband', 'fiancee', 'partner'], 'solo' : ['solo', 'single', 'individual'], 'pet_friendly' : ['dog', 'pet', 'cat'], 'disabled' : ['disabled', 'handicap', 'accessible'], 'LGBTQI' : ['gay', 'lesbian', 'lgbt', 'lgbtqia', 'lgbtqi'], 'senior' : ['senior', 'pensioner', 'elderly'], 'adult' : ['adult'], 'young' : ['young', '18-30', 'under 30'], 'under_21' : ['under 21', 'under age', '18 year old'], 'over_x' : ['over 40', 'over 50', 'over 60', 'over 70', 'over 80'], 'group' : ['group', 'small group', 'guests'], 'veteran' : ['veteran']},
+
+        'topic': {'jet lag': ['jet lag']},
+        
+        'modifier' : {'best': ['top', 'best', 'great'], 'cheap': ['cheap', 'budget', 'affordable'], 'luxury': ['luxury', 'exclusive']},
+
+        'destinations': {'usa': ['usa'], 'united kingdom': ['united kingdom'], 'spain':['spain']}
+            
+    }
